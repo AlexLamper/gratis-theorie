3f2db975e37e410cf7aa3a8e56dbf1261b7c3317
Alex <lamp0020@hz.nl>
2025-07-08 23:32:17 +0200

    Initial Commit (NextJS install

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold mb-8">Gratis Theorie</h1>
+      <p>Leer gratis voor je theorie examen.</p>
+    </main>
+  );
+}
